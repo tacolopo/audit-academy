f4e54315d8d9b7fee7ed060a05a546076b57748b
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-09-06 15:11:08 -0400

    add tx

--- configure_environment/configure_environment.py
+++ configure_environment/configure_environment.py
@@ -5,4 +5,5 @@ os.system('''
 	AKASH_VERSION="$(curl -s "$AKASH_NET/version.txt")";
 	export AKASH_CHAIN_ID="$(curl -s "$AKASH_NET/chain-id.txt")";
 	export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)";
+	akash query tx ACF35CCBD4DA831471C77943994C9040E88E88069F6693B666B5F78880FBEB9A;
 ''')
