cf9c25aef688f7dd20ef645af5d248f0fb3d5b95
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-09-06 14:55:33 -0400

    Create configure_environment.py

---
+++ configure_environment/configure_environment.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('''
+	brew install ovrclk/tap/akash;
+	AKASH_NET="https://raw.githubusercontent.com/ovrclk/net/master/mainnet";
+	AKASH_VERSION="$(curl -s "$AKASH_NET/version.txt")";
+	export AKASH_CHAIN_ID="$(curl -s "$AKASH_NET/chain-id.txt")";
+	export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)";
+''')
