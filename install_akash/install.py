218a9f47a92b3926ac1219fdc7902aedd59d2cf9
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-09-06 15:02:37 -0400

    Update install.py

--- install_akash/install.py
+++ install_akash/install.py
@@ -6,4 +6,5 @@ os.system(f'''
 	AKASH_VERSION="$(curl -s "$AKASH_NET/version.txt")";
 	export AKASH_CHAIN_ID="$(curl -s "$AKASH_NET/chain-id.txt")";
 	export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)";
+	akash version;
 ''')
